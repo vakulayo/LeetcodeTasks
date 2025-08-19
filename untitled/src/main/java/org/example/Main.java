bdf7c408b1d5e46a116bd18c605875fcf6f4ea5d
ekaterina <vakula.yo@gmail.com>
2025-08-19 21:07:38 +0300

    noref num 2 leetcode

--- untitled/src/main/java/org/example/Main.java
+++ untitled/src/main/java/org/example/Main.java
@@ -1,5 +1,8 @@
 package org.example;
 
+import java.util.Arrays;
+import java.util.List;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -8,7 +11,7 @@ public class Main {
 
         Solution solution = new Solution();
         //test1:
-        int[] nums1 = {0,1,2,2,3,0,4,2};
+        int[] nums1 = {0, 1, 2, 2, 3, 0, 4, 2};
         int val1 = 2;
         System.out.println(solution.removeElement(nums1, val1) + " //Should be 5");
 
@@ -27,5 +30,36 @@ public class Main {
         int[] nums4 = {2};
         int val4 = 2;
         System.out.println(solution.removeElement(nums4, val4) + " //Should be 0");
+
+
+        //test1:
+        ListNode l11 = createLinkedList(Arrays.asList(3, 4, 2));
+        ListNode l21 = createLinkedList(Arrays.asList(4, 6, 5));
+        ListNode listNode1 = solution.addTwoNumbers(l11, l21); //Should be [7, 0, 8]
+
+        //test2:
+        ListNode l12 = createLinkedList(List.of(0));
+        ListNode l22 = createLinkedList(List.of(0));
+        ListNode listNode2 = solution.addTwoNumbers(l12, l22); //Should be [0]
+
+        //test3:
+        ListNode l13 = createLinkedList(Arrays.asList(9,9,9,9,9,9,9));
+        ListNode l23 = createLinkedList(Arrays.asList(9,9,9,9));
+        ListNode listNode3 = solution.addTwoNumbers(l13, l23); //Should be [0]
+
+
+    }
+
+    public static ListNode createLinkedList(List<Integer> l) {
+        ListNode result = null;
+
+        for (Integer elem : l) {
+            ListNode newNode = new ListNode();
+            newNode.val = elem;
+            newNode.next = result;
+            result = newNode;
         }
+
+        return result;
+    }
 }
--- untitled/src/main/java/org/example/Solution.java
+++ untitled/src/main/java/org/example/Solution.java
@@ -1,5 +1,9 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Solution {
 
     // https://leetcode.com/problems/remove-element/description/ problem #27
@@ -33,5 +37,50 @@ public class Solution {
         } else {
             return i + 1;
         }
+    }
+
+    // https://leetcode.com/problems/add-two-numbers/description/ problem #2
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+        List<Integer> result = new ArrayList<>();
+        boolean additionalOne = false;
+
+        while (l1 != null || l2 != null) {
+
+            int sum = 0;
+            if (l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+
+            if (l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+
+            if (additionalOne) {
+                sum++;
+                additionalOne = false;
+            }
+            if (sum > 9) {
+                additionalOne = true;
+                result.add(sum % 10);
+            } else {
+                result.add(sum);
+            }
+        }
+
+        if (additionalOne) {result.add(1);}
+
+        Collections.reverse(result);
+        ListNode resultList = null;
+        for (Integer elem : result) {
+            ListNode newNode = new ListNode();
+            newNode.val = elem;
+            newNode.next = resultList;
+            resultList = newNode;
+        }
+
+        return resultList;
     }
 }
